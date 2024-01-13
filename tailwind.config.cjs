988c2e47bf96b493190e2e3116f2c84ba7e5ec7b
Dharmzeey <olanrewajuadam2020@gmail.com>
2024-01-13 02:35:01 +0100

    animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes:{
+				zoom_in:{
+					'0%': {transform: 'scale(0)', opacity: '0'},
+					'100': {transform: 'scale(1)', opacity: '1'}
+				}
+			},
+			animation: {
+				zoom_in: 'zoom_in ease-in 1s',
+			}
+		},
 	},
 	plugins: [],
 }
