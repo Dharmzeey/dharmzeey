006d7fce1d80945744b4d3ca6108292b2b80a1b3
Adam Olanrewaju <olanrewajuadam2020@gmail.com>
2025-10-13 00:13:43 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dharmzeey.com',
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto'
-  },
-  vite: {
-    plugins: [tailwindcss()],
+    site: 'https://dharmzeey.com',
+    compressHTML: true,
     build: {
-      cssMinify: true,
-      minify: 'terser',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['astro']
-          }
+        inlineStylesheets: 'auto'
+    },
+    integrations: [sitemap(
+        {
+            changefreq: 'weekly',
+            lastmod: new Date(),
+            priority: 0.9,
         }
-      }
-    }
-  },
-  adapter: netlify({
-    edgeMiddleware: false
-  })
+    )],
+    vite: {
+        plugins: [tailwindcss()],
+        build: {
+            cssMinify: true,
+            minify: 'terser',
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vendor: ['astro']
+                    }
+                }
+            }
+        }
+    },
+    adapter: netlify({
+        edgeMiddleware: false
+    })
 });
